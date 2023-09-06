6b5c80314d430b00c01d4578d5be43bfca5e2753
Ignacio <ignaciomarucco@gmail.com>
2023-09-06 20:22:09 -0300

    Add changedisplay, newoperator, newnumber functions

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 "use strict"; 
-
+// 
+let operation = {
+  firstOperand: '',
+  secondOperand: '',
+  operator: null,
+  result: null,
+}
 function operate(operator, firstOperand, secondOperand) {
   let result;
   switch (operator) {
@@ -18,8 +24,11 @@ function operate(operator, firstOperand, secondOperand) {
     case "/":
       result = divide(firstOperand, secondOperand);
       break;
+    
+    default:
+      return null;
   }
-  return result; 
+  operation["result"] = result;
 }
 
 // Basic Operations Functions
@@ -37,4 +46,49 @@ function multiply(a, b) {
 
 function divide(a, b) {
   return a / b;
+}
+
+// DOM
+const display = document.querySelector(".display");
+const numbersBtns = document.querySelectorAll("[data-number]");
+numbersBtns.forEach(numberBtn => numberBtn.addEventListener("click", newNumber));
+const operatorsBtns = document.querySelectorAll("[data-operator]");
+operatorsBtns.forEach(operatorBtn => operatorBtn.addEventListener("click", newOperator));
+const equalBtn = document.querySelector("[data-equal]");
+equalBtn.addEventListener("click", () => {
+  operate(operation.operator, +operation.firstOperand, +operation.secondOperand);
+  changeDisplay(operation["result"]);
+});
+
+// Function to Save Number
+function newNumber(e) {
+  console.log(e.target.textContent);
+  if (operation["operator"] === null) {
+    operation["firstOperand"] += e.target.textContent;
+    changeDisplay(operation["firstOperand"])
+  } else {
+    operation["secondOperand"] += e.target.textContent;
+    changeDisplay(operation["secondOperand"])
+  }
+}
+
+// Function to Save Operator
+function newOperator(e) {
+  console.log(e.target.textContent);
+  if (operation["operator"] !== null) {
+    operate(operation.operator, +operation.firstOperand, +operation.secondOperand);
+    changeDisplay(operation["result"]);
+
+    operation["firstOperand"] = operation["result"];
+    operation["secondOperand"] = '';
+  }
+  
+  operation["operator"] = e.target.textContent;
+
+
+}
+
+// Function to change display
+function changeDisplay(element) {
+  display.textContent = element;
 }
